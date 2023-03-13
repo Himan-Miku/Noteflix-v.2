7126c122cafc802b1386f2d7f85154b1210734ee
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-13 19:44:38 +0530

    feat: add firebase-config 🔥

---
+++ config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBT7mAxpH33FU2R1oxN4ZWuUMNlqHJNpE0",
+  authDomain: "noteflix-621dd.firebaseapp.com",
+  projectId: "noteflix-621dd",
+  storageBucket: "noteflix-621dd.appspot.com",
+  messagingSenderId: "280443811042",
+  appId: "1:280443811042:web:a13f445df0399c6f749abb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
