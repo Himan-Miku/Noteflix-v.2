c5904e74b9cdbced13fa6d1b32edaffd544ef83b
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-15 17:06:31 +0530

    pack: removed zustand

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBT7mAxpH33FU2R1oxN4ZWuUMNlqHJNpE0",
@@ -12,5 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const githubProvider = new GithubAuthProvider();
+export const db = getFirestore(app);
