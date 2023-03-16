1cc0a3e63aab87634708a8909a479646d452e84d
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-16 14:35:49 +0530

    pack: add react-icons 🪄

--- app/Landing.tsx
+++ app/Landing.tsx
@@ -2,10 +2,8 @@ import Login from "@/components/Login";
 
 const Landing = () => {
   return (
-    <div className="h-screen justify-center items-center">
-      <div className="w-56">
-        <Login />
-      </div>
+    <div className="h-screen justify-center items-center bg-[#A9A9A9]">
+      <Login />
     </div>
   );
 };
--- components/Login.tsx
+++ components/Login.tsx
@@ -4,9 +4,19 @@ import { signIn } from "next-auth/react";
 
 const Login = () => {
   return (
-    <div className="flex items-center justify-center gap-y-8">
-      <button onClick={() => signIn()}>Sign In With Google</button>
-      <button onClick={() => signIn()}>Sign In With Github</button>
+    <div className="flex flex-col items-center h-screen justify-center gap-y-8">
+      <button
+        className="min-w-max w-max px-3 py-4 rounded-lg "
+        onClick={() => signIn()}
+      >
+        <p className="text-white">Sign In With Google</p>
+      </button>
+      <button
+        className="min-w-max w-max px-3 py-4 rounded-lg bg-[#22272B]"
+        onClick={() => signIn()}
+      >
+        <p className="text-white">Sign In With Github</p>
+      </button>
     </div>
   );
 };
