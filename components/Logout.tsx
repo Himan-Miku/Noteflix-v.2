2d3ef31ebb90698ce21493b573fa83e0c5def7de
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-18 23:39:04 +0530

    style: changed logout button style 🪄

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -22,7 +22,7 @@ const Logout = ({ image }: UserProps) => {
         />
       </Link>
       <button
-        className="px-4 py-2 text-gray-300 rounded-lg border border-1 border-[#575B5F] m-1 w-full bg-[#9f4040a7] transition-all duration-300 hover:bg-[#9f4040c7] hover:-translate-y-[1px]"
+        className="px-4 py-2 text-gray-300 rounded-lg font-medium m-1 w-full bg-[#9f4040a7] transition-all duration-300 hover:bg-[#9f4040c7] hover:-translate-y-[1px]"
         onClick={() => signOut()}
       >
         Sign Out
