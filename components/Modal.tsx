3c2e9327226a5f0781c71efd88f02a09c6b7c751
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-23 23:10:44 +0530

    style: modal 🪄

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,9 +1,32 @@
+"use client";
+
 import { iModalContext, modalContext } from "@/context/ModalContext";
 import { Dialog, Transition } from "@headlessui/react";
-import React, { Fragment } from "react";
+import React, { ChangeEvent, Fragment, useState } from "react";
 
 const Modal = () => {
   const { isOpen, closeModal } = modalContext() as iModalContext;
+  const [rows, setRows] = useState(1);
+  const [inputs, setInputs] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleTextAreaInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputs({ ...inputs, content: e.target.value });
+    console.log(e.target.scrollHeight, " => height");
+    console.log(e.target.value.length, " => inputLength");
+
+    if (e.target.scrollHeight <= 64 || inputs.content.length <= 29) setRows(1);
+    else if (e.target.scrollHeight === 88 || inputs.content.length <= 58)
+      setRows(2);
+    else if (e.target.scrollHeight === 112 || inputs.content.length <= 87)
+      setRows(3);
+    else if (e.target.scrollHeight === 136 || inputs.content.length <= 116)
+      setRows(4);
+    else if (e.target.scrollHeight >= 160 || inputs.content.length >= 145)
+      setRows(5);
+  };
 
   return (
     <div>
@@ -18,7 +41,7 @@ const Modal = () => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className="fixed inset-0 bg-black bg-opacity-50" />
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
@@ -33,17 +56,27 @@ const Modal = () => {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    Payment successful
+                  <Dialog.Title as="h3">
+                    <input
+                      className="bg-transparent px-4 py-2 focus:outline-none w-full text-white font-semibold placeholder:font-semibold caret-white"
+                      placeholder="Title"
+                      value={inputs.title}
+                      onChange={(e) =>
+                        setInputs({ ...inputs, title: e.target.value })
+                      }
+                      required
+                    />
                   </Dialog.Title>
                   <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Your payment has been successfully submitted. We’ve sent
-                      you an email with all of the details of your order.
-                    </p>
+                    <textarea
+                      className={`bg-[#202124] px-4 py-2 mt-1 focus:outline-none w-full placeholder:font-semibold placeholder:text-sm caret-white resize-none overflow-hidden h-auto max-h-40 text-white`}
+                      placeholder="Take a note..."
+                      value={inputs.content}
+                      onChange={(e) => handleTextAreaInput(e)}
+                      rows={rows}
+                      autoFocus
+                      required
+                    />
                   </div>
 
                   <div className="mt-4">
@@ -52,7 +85,7 @@ const Modal = () => {
                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeModal}
                     >
-                      Got it, thanks!
+                      close
                     </button>
                   </div>
                 </Dialog.Panel>
