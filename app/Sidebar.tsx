0bf175e66f88df1693111fb7350bcb0196d34039
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-31 01:40:17 +0530

    style : alert for archive 🪄

--- app/Sidebar.tsx
+++ app/Sidebar.tsx
@@ -30,8 +30,8 @@ const sideBar = [
 const Sidebar = () => {
   const pathname = usePathname();
   return (
-    <div>
-      <div className="cal-h w-72 py-2 overflow-y-auto bg-[#202124]">
+    <>
+      <div className="cal-h w-72 py-2 overflow-y-auto bg-[#202124] relative">
         {sideBar.map((item, index) => (
           <Link key={index} href={item.path}>
             <div
@@ -56,8 +56,52 @@ const Sidebar = () => {
             </div>
           </Link>
         ))}
+        <div
+          id="alert-5"
+          className="flex p-4 rounded-lg bg-[#73353693] absolute bottom-4 left-10"
+          role="alert"
+        >
+          <svg
+            aria-hidden="true"
+            className="flex-shrink-0 w-5 h-5 text-gray-300 dark:text-gray-300"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span className="sr-only">Info</span>
+          <div className="ml-3 text-sm font-medium text-gray-300 dark:text-gray-300">
+            Note Archived
+          </div>
+          <button
+            type="button"
+            className="ml-3 -mx-1.5 -my-1.5 transition-colors duration-300 bg-transparent text-gray-300 rounded-lg p-1.5 hover:bg-[#733536d0] inline-flex h-8 w-8"
+            data-dismiss-target="#alert-5"
+            aria-label="Close"
+          >
+            <span className="sr-only">Dismiss</span>
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
