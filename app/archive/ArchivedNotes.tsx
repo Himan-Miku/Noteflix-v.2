083a3a90dac0d937a803e293c74269bd17292f20
Himan-Miku <himanshu28.mj@gmail.com>
2023-11-11 00:00:56 +0530

    style: add Archived Notes heading 🪄

--- app/archive/ArchivedNotes.tsx
+++ app/archive/ArchivedNotes.tsx
@@ -7,20 +7,13 @@ import Note from "@/components/Note";
 import { NoteContextProvider } from "@/context/NoteContext";
 import Modal from "@/components/Modal";
 import { useSession } from "next-auth/react";
+import { noteData } from "../Notes";
 
 export interface iNote {
   title: string;
   content: string;
   userId: string;
   bgImage?: string;
-}
-
-export interface noteData {
-  title: string;
-  content: string;
-  bgImage?: string;
-  opImage?: string;
-  archived: boolean;
 }
 
 export default function ArchivedNotes() {
@@ -37,11 +30,16 @@ export default function ArchivedNotes() {
 
   return (
     <NoteContextProvider>
-      <div ref={parent} className="notes-columns gap-4 p-4 my-8">
-        {notes?.docs.map((note) => (
-          <Note key={note.id} id={note.id} data={note.data() as noteData} />
-        ))}
-        <Modal />
+      <div className="flex flex-col gap-2">
+        <div className="text-[#DA7370] font-semibold text-2xl px-4">
+          <h3>Archived Notes : </h3>
+        </div>
+        <div ref={parent} className="notes-columns gap-4 p-4 my-4">
+          {notes?.docs.map((note) => (
+            <Note key={note.id} id={note.id} data={note.data() as noteData} />
+          ))}
+          <Modal />
+        </div>
       </div>
     </NoteContextProvider>
   );
