20663893a9a02a54aa994a65871ee0b1367a7779
Himan-Miku <himanshu28.mj@gmail.com>
2023-11-11 19:42:33 +0530

    refactor: rm reminders sidebar item 🪛

--- app/Sidebar.tsx
+++ app/Sidebar.tsx
@@ -32,13 +32,6 @@ const sideBar: {
     ),
   },
   {
-    title: "Reminders",
-    path: "/reminders",
-    svgPath: (
-      <path d="M18 17v-6c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v6H4v2h16v-2h-2zm-2 0H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6zm-4 5c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2z"></path>
-    ),
-  },
-  {
     title: "Archive",
     path: "/archive",
     svgPath: (
