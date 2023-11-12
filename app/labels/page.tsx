967fb4d20ed502a08a7c21def914a7bcf45e9c7c
Himan-Miku <himanshu28.mj@gmail.com>
2023-11-12 22:35:54 +0530

    fix: add if condition to check if noteRefs are undefined or not 🪛

--- app/labels/page.tsx
+++ app/labels/page.tsx
@@ -9,8 +9,6 @@ import {
   deleteDoc,
   doc,
   getDoc,
-  query,
-  where,
   writeBatch,
 } from "firebase/firestore";
 import { ChangeEvent, useState } from "react";
@@ -57,19 +55,21 @@ export default function LabelsPage() {
     let labelSnapshot = await getDoc(docRef);
 
     let labelSnapData = labelSnapshot.data() as labelDataWithoutID;
-    labelSnapData.noteRefs.forEach((noteId) => {
-      const noteRef = doc(db, "notes", noteId);
-      batch.update(noteRef, {
-        labels: arrayRemove(labelSnapData.labelName),
+    if (labelSnapData.noteRefs) {
+      labelSnapData.noteRefs.forEach((noteId) => {
+        const noteRef = doc(db, "notes", noteId);
+        batch.update(noteRef, {
+          labels: arrayRemove(labelSnapData.labelName),
+        });
       });
-    });
-    await batch.commit();
+      await batch.commit();
+    }
     await deleteDoc(doc(db, "labels", id));
   };
 
   return (
     <main
-      className="cal-h overflow-y-auto overflow-x-hidden scrollbar px-20 pt-10 pb-8"
+      className="cal-h overflow-y-auto overflow-x-hidden scrollbar md:px-20 px-10 md:pt-10 pt-6 md:pb-8 pb-6"
       style={{
         backgroundImage: `url("/note-page-2.png")`,
         backgroundSize: "cover",
@@ -85,56 +85,58 @@ export default function LabelsPage() {
             backgroundPositionY: "top",
             backgroundSize: "cover",
           }}
-          className="w-1/3 flex flex-col gap-4 justify-center items-center rounded-lg py-2"
+          className="md:w-1/3 w-5/6 flex flex-col md:gap-4 gap-3 justify-center items-center rounded-lg py-2"
         >
-          <div className="text-2xl font-semibold text-[#9F4040] pt-2">
+          <div className="md:text-2xl text-xl font-semibold text-[#9F4040] pt-2">
             <h3>Edit Labels :</h3>
           </div>
-          <div className="flex gap-2 min-w-fit items-center px-2 py-1">
-            <div
-              onClick={createNewLabel}
-              className="rounded-full p-1 text-[#DA7370] hover:bg-[#2f3033]"
-            >
-              <AiOutlinePlus />
+          <div className="w-[20rem] flex flex-col md:gap-3 gap-2">
+            <div className="flex md:gap-3 gap-4 min-w-fit w-full items-center px-2 py-1 justify-center">
+              <div
+                onClick={createNewLabel}
+                className="rounded-full p-1 text-[#DA7370] hover:bg-[#2f3033]"
+              >
+                <AiOutlinePlus />
+              </div>
+              <input
+                type="text"
+                id="labelName"
+                autoComplete="off"
+                placeholder="Create new label"
+                className="bg-[#2f3033] text-[#e6e6e6] outline-none px-2 py-1 caret-gray-400 rounded-md placeholder:text-white"
+                value={labelName}
+                onChange={handleChange}
+              />
+              <div
+                onClick={createNewLabel}
+                className="rounded-full p-1 text-[#DA7370] hover:bg-[#2f3033]"
+              >
+                <BsCheckLg />
+              </div>
             </div>
-            <input
-              type="text"
-              id="labelName"
-              autoComplete="off"
-              placeholder="Create new label"
-              className="bg-[#2f3033] text-[#e6e6e6] outline-none px-2 py-1 caret-gray-400 rounded-md placeholder:text-white"
-              value={labelName}
-              onChange={handleChange}
-            />
-            <div
-              onClick={createNewLabel}
-              className="rounded-full p-1 text-[#DA7370] hover:bg-[#2f3033]"
-            >
-              <BsCheckLg />
-            </div>
-          </div>
-          <div className="w-[18.3rem] min-w-fit">
-            {labels &&
-              labels.map((label, index) => (
-                <div
-                  key={index}
-                  className="flex gap-2 items-center justify-between py-2 px-1"
-                >
-                  <div className="flex gap-4 font-medium items-center">
-                    <div className="p-1 rounded-full">
-                      <MdLabel size={"1.2em"} />
-                    </div>
-
-                    <p>{label.title}</p>
-                  </div>
+            <div className="w-full min-w-fit pl-3 pr-2">
+              {labels &&
+                labels.map((label, index) => (
                   <div
-                    onClick={() => deleteLabel(label.id)}
-                    className="p-1 rounded-full bg-transparent transition-all duration-300 ease-in-out hover:bg-[#DA7370] hover:text-white cursor-pointer"
+                    key={index}
+                    className="flex gap-2 items-center justify-between py-2 px-1"
                   >
-                    <MdDelete size={"1.2em"} />
+                    <div className="flex gap-4 font-medium items-center">
+                      <div className="p-1 rounded-full">
+                        <MdLabel size={"1.2em"} />
+                      </div>
+
+                      <p>{label.title}</p>
+                    </div>
+                    <div
+                      onClick={() => deleteLabel(label.id)}
+                      className="p-1 rounded-full bg-transparent transition-all duration-300 ease-in-out hover:bg-[#DA7370] hover:text-white cursor-pointer"
+                    >
+                      <MdDelete size={"1.2em"} />
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+            </div>
           </div>
         </div>
       </div>
