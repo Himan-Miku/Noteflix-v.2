540dfd1c87f263c7e22a3175f20c638f5fe26568
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-04-23 12:19:08 +0530

    fix: undefined on replace method

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,7 +9,9 @@ export const authOptions: NextAuthOptions = {
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID!,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+        : undefined,
     }),
   }),
   secret: process.env.NEXTAUTH_SECRET,
