f8aad1214a64171e01757dfcc3f90f810d58febb
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-04-25 10:54:23 +0530

    test: create .env file in github actions

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,7 +5,13 @@ import { cert } from "firebase-admin/app";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 
 export const authOptions: NextAuthOptions = {
-  adapter: FirestoreAdapter(),
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID!,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
+  }),
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
