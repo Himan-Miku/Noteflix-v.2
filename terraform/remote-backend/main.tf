2414f001c94ad18ed2f63666331dc28b1c9e78a3
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-04-28 16:59:44 +0530

    ci: setup tf with remote backend

---
+++ terraform/remote-backend/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  backend "s3" {
+    bucket         = "noteflix-remote-backend"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "noteflix-remote-backend-lock"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.46.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "noteflix_backend_bucket" {
+  bucket        = "noteflix-remote-backend"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "noteflix_bucket_versioning" {
+  bucket = aws_s3_bucket.noteflix_backend_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "noteflix_backend_bucket_conf" {
+  bucket = aws_s3_bucket.noteflix_backend_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "noteflix_backend_lock" {
+  name         = "noteflix-remote-backend-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
