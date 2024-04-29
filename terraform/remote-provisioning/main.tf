6aa593425d8634c9247beffd0755d6ac6f48e3bc
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-04-29 23:22:38 +0530

    refactor: rm outputs.tf

--- terraform/remote-provisioning/main.tf
+++ terraform/remote-provisioning/main.tf
@@ -70,3 +70,7 @@ resource "aws_instance" "ec2_aws_instance" {
 output "ec2_public_dns" {
   value = aws_instance.ec2_aws_instance.public_dns
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.ec2_aws_instance.public_ip
+}
--- terraform/remote-provisioning/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ec2_public_ip" {
-  value = aws_instance.ec2_aws_instance.public_ip
-}
