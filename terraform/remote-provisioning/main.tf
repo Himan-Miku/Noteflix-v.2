22e4f189bff398da2f45d7763e8c8d988b265e48
Himanshu Jagdale <himanshu28.mj@gmail.com>
2024-05-04 17:10:33 +0530

    test

--- terraform/remote-provisioning/main.tf
+++ terraform/remote-provisioning/main.tf
@@ -1,3 +1,9 @@
+locals {
+  private_key_path = "~/.ssh/ansible-ssh.pem"
+  key_name         = "ansible-ssh"
+  ssh_user         = "ec2-user"
+}
+
 terraform {
   backend "s3" {
     bucket         = "noteflix-remote-backend"
@@ -65,12 +71,18 @@ resource "aws_instance" "ec2_aws_instance" {
   tags = {
     Name = "terraform-ec2-instance"
   }
+  key_name = local.key_name
 
-  user_data = <<EOF
-#!/bin/bash
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC2R9aW1ebCa6nrBEjvZ6NK5Nff0i7czO4mmHmqOCJS8V04Iy2U1xPRO3rHebS5FBAyw+X0wpg7GcCoIQ3loK1AKOq7Jhy+al3v8K6LRPz1eZLilUp16iIOaJ0uDz0Loik834iYxBNTSWK1+gJ0Lq090uwSx1LoWO0vnwoJMbwP/CERXi1AQAeFUNPs5dcEXP2PYyx8yAzxm7WoPw58XHC6t/AExSUPztx093F/r/ohTdMNpzYs12oltJOwFm+8U4PIMA1yumFSqR4xNCsK2VRzOrG65pcd1wXGGKQwO685FlVN16b4wuXz5YlDZK4O/N4Kp/TKjq2jLv9CHcXYDSdpIhojybfsL067l75SPbLqkcRBtPAu0BYEwVlSwz0deE2MExQOIjgcDQRMCiDR5cc485OHbsKyqw/abpfnsGSyOGg6Ckvi8LbDMcf0CHdQDZ7jdv/m/mInZhNpYf1UJK/WX83bPJst+uTdpPskf+WM8UGmlYDsaQkCMDHbLHANMgE= miku@himan" >> ~/.ssh/authorized_keys
-chmod 0600 ~/.ssh/authorized_keys
-EOF
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = self.public_ip
+    }
+  }
 }
 
 output "ec2_public_dns" {
